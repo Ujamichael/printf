d2d2f2a863229499aa3296ee7700cd8808a127e3
ujamichael <ujahhrmajesti@gmail.com>
2023-11-11 23:22:46 -0800

    custom printf function, task 0

--- printf.c
+++ printf.c
@@ -12,9 +12,7 @@ int _printf(const char *format, ...)
 	va_list my_args;
 
 	if (format == NULL)
-	{
 		return (-1);
-	}
 
 	va_start(my_args, format);
 
@@ -30,28 +28,20 @@ int _printf(const char *format, ...)
 			format++;
 
 			if (*format == '\0')
-			{
 				break;
-			}
 			if (*format == 'c')
-			{
 				c = va_arg(my_args, int);
 				write(1, &c, 1);
 				print_char++;
-			}
 			if (*format == '%')
-			{
 				write(1, format, 1);
 				print_char++;
-			}
 			if (*format == 's')
-			{
 				str = va_arg(my_args, char*);
 				while (str[_strlen] != '\0')
 					_strlen++;
 				write(1, str, _strlen);
 				print_char += _strlen;
-			}
 		}
 		format++;
 	}
