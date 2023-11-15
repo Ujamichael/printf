ad47a2fc07ac37a8feb7c5ae86eb6a2e1e7894a8
9ightshade <aniekemeusanga@gmail.com>
2023-11-15 04:40:34 -0800

    _printf function

---
+++ printf.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * _printf() - prints output according to a format
+ * @fromat: format string with characters and specifiers
+ * des:this function will call get_print() that will 
+ * determine which printing function to call depending on the
+ * specifiers and return the lenght of the formatted string
+ */
+int _printf(const char *format, ...)
+{
+	int(*pfunc)(va_list, flags_s *);
+	const char *p;
+	va_list args;
+	flags_s flags = {0, 0, 0};
+
+	register int counter = 0;
+
+	va_start(args, format);
+	if(!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if(format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
+	{
+		if(*p == '%')
+		{
+			p++;
+			if (*p == '%')
+			{
+				counter += _putchar('%');
+				continue;
+			}		
+			while (get_flag(*p, &flags))
+			p++;
+			pfunc = get_print(*p);
+			counter += (pfunc)
+				?pfunc(args, &flags)
+				: _printf("%%%c", *p);
+		}
+		else
+			counter += _putchar(*p);
+	}
+	_putchar(-1);
+	va_end(args);
+	return(counter);
+}
