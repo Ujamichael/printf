70e1a29941f539d1dc4e6ea34c0a6fd07a7c5033
9ightshade <aniekemeusanga@gmail.com>
2023-11-15 06:01:15 -0800

    va_list update

--- main.h
+++ main.h
@@ -35,16 +35,16 @@ typedef struct handleprint
 }hp;
 
 /*print_number */
-int print_int(va_list 1, flags_s *f);
+int print_int(va_list len, flags_s *f);
 void print_numb(int n);
-int print_unsigned(va_list 1, flags_s *f);
+int print_unsigned(va_list len, flags_s *f);
 int digit_counter(int i);
 
 /*print_bases*/
-int print_hex(va_list 1, flags_s *f);
-int print_hex_lg(va_list 1, flags_s *f);
-int print_binary(va_list 1, flags_s f*);
-int print_octal(va_list 1, flags_s *f);
+int print_hex(va_list len, flags_s *f);
+int print_hex_lg(va_list len, flags_s *f);
+int print_binary(va_list len, flags_s f*);
+int print_octal(va_list len, flags_s *f);
 
 /*converter*/
 char *converter(unsigned long int numb, int base, int lowercase);
@@ -56,8 +56,8 @@ int _printf(const char *format, ...);
 int get_flag(char s, flags_s *f);
 
 /*print_alpha */
-int print_strings(va_list 1, flags_s *f);
-int print_char(va_list 1, flags_s *f);
+int print_strings(va_list len, flags_s *f);
+int print_char(va_list len, flags_s *f);
 
 /*get_print*/
 int (*get_print(char s))(va_list, flags_s *);
@@ -67,16 +67,16 @@ int _putchar(char c);
 int _puts(char *str);
 
 /*print_custom */
-int print_rot13(va_list 1, flags_s *f);
-int print_rev(va_list 1, flags_s *f);
-int print_bigs(va_list 1, flags_s *f);
+int print_rot13(va_list len, flags_s *f);
+int print_rev(va_list len, flags_s *f);
+int print_bigs(va_list len, flags_s *f);
 
 
 /*print_address*/
-int print_address(va_list 1, flags_s *f);
+int print_address(va_list len, flags_s *f);
 
 /*print_percent*/
-int print_percent(va_list 1, flags_s *f);
+int print_percent(va_list len, flags_s *f);
 
 
 #endif
